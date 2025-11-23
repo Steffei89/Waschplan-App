62eac40ba97706bdf45b7f52108f15b95d535ba0
Stefan Reichart <sr1989@icloud.com>
2025-11-23 19:49:05 +0100

    2.9.0.1

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 export const SECRET_INVITE_CODE = "FamRei2025"; 
 // ==========================
 
-export const APP_VERSION = "2.8.1"; // Stats Update
+export const APP_VERSION = "2.9.0"; // Stats Update
 
 // ===== Wasch-Karma Einstellungen =====
 export const KARMA_START = 100;       
